5dad8b9aed07e41d70a3f5f67dd5e81d796e9a1d
bird <gcinumuzimothusibird@gmail.com>
2026-02-21 08:24:34 +0000

    and?

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,6 @@ export const firebaseConfig = {
   "appId": "1:302023521826:web:aff691e0aea22ccc4c3c7b",
   "apiKey": "AIzaSyBk3PRvKDGzlyVmQbs8pe8DovXdqzvUKRc",
   "authDomain": "studio-1397497397-8a393.firebaseapp.com",
-  "measurementId": "",
+  "storageBucket": "studio-1397497397-8a393.firebasestorage.app",
   "messagingSenderId": "302023521826"
 };
